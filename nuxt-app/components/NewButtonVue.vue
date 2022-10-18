c8b987a7d2bc488f28e8ba3914b6de9ba1925e10
Andrea Bertoncini <software2@pollinisrl.it>
2022-10-18 15:07:55 +0200

    commit prova

---
+++ nuxt-app/components/NewButtonVue.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button" @click="hello">Pulsante prova button</button>
+  {{ message }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    hello() {
+      this.message = "hello";
+    },
+  },
+};
+</script>
